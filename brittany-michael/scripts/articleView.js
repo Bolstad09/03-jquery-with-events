f131165eae790ec9c4a817d2469d8e857ae25863
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-03-22 19:41:49 -0700

    some assistance from code review

--- brittany-michael/scripts/articleView.js
+++ brittany-michael/scripts/articleView.js
@@ -22,7 +22,7 @@ articleView.populateFilters = function() {
 
       // REVIEW: Similar to the above, but...
       // Avoid duplicates! We don't want to append the category name if the <select> already has this category as an option!
-      category = $(this).attr('#category-filter');
+      category = $(this).attr('data-category');
 
       // TODO: Refactor this concatenation using a template literal.
       optionTag = `<option value="${category} ">${category}</option>`;
@@ -37,15 +37,16 @@ articleView.populateFilters = function() {
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
-    if ($(this).val()) {
+    if ($(this).val()) { //checks if there is a value there, if nothing(falsey) then it just moves onto the else below
       // TODONE: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
 
-      $('article').hide()
-      $('article[data-author="' + ($(this).val()) + '"]').fadeIn('slow');
+      $('article').hide();
+      $('article[data-author="' + ($(this).val()) + '"]').fadeIn('slow'); //should we or do we need to add .filter after 'article'
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
-
+      $('article').show();
+      $('article.template').hide();
     }
     $('#category-filter').val('');
   });
@@ -61,9 +62,10 @@ articleView.handleCategoryFilter = function() {
       $('article').hide()
       $('article[data-category="' + ($(this).val()) + '"]').fadeIn('slow');
     } else {
-      
+      $('article').show();
+      $('article.template').hide();
     }
-    $('#category-filter').val('');
+    $('#author-filter').val('');
   });
 };
 
@@ -71,8 +73,10 @@ articleView.handleMainNav = function() {
   // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-  $('.main-nav').on()('click','.tab', function(){
-    $('.tab-content').hide()
+  $('.main-nav').on()('click','.tab', function(e){
+    e.preventDefault();
+    $('.tab-content').hide();
+    $(`#${$(this).data('content')}`).fadeIn();
   })
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
   $('.main-nav .tab:first').click();
@@ -84,6 +88,12 @@ articleView.setTeasers = function() {
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+
+  $('#articles').on('click', 'read-on', function(e){  //need to review  *(means any)
+    e.preventDefault();
+    $(this).parent().find('.article-body *').fadeIn();
+    $(this).hide();  //someone used .toggle
+  })
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
